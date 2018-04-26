fb254dd52b74a7173696a48f285dec8daf8a7d36
Jacolby Green <darknightnov@gmail.com>
2018-04-26 15:54:43 -0500

    added style to Itinerary Items

--- client/src/components/Itinerary.js
+++ client/src/components/Itinerary.js
@@ -10,16 +10,32 @@ export default class ItineraryList extends React.Component {
     this.state = {
       trips: [
         {
-          flight: "Dallas to Maui"
+          flight: "Dallas to Maui",
+          total: '$2500',
+          flightNumber: 3340,
+          airline: "American Airlies",
+          hotel: "Sufer's Cove Hotel"
         },
         {
-          flight: "Houston to Brazil"
+          flight: "Houston to Brazil",
+          total: '$1500',
+          flightNumber: 3564,
+          airline: "American Airlies",
+          hotel: "DoubleTree"
         },
         {
-          flight: "New Orleans to Morroco"
+          flight: "New Orleans to Morroco",
+          total: '$2500',
+          flightNumber: 32340,
+          airline: "Sun Fligh International",
+          hotel: "MoonSun Hotel"
         },
         {
-          flight: "Shreveport to Winnipeg"
+          flight: "Shreveport to Winnipeg",
+          total: '$2500',
+          flightNumber: 67340,
+          airline: "American Airlies",
+          hotel: "Marriot"
         }
       ],
       user: this.props.user
@@ -36,7 +52,11 @@ componentWillMount() {
     console.log(this.state)
     const letsGo = this.state.trips.map((trip, i) =>
       <ItineraryItem key={i}
-                     destination={trip.flight}/>
+                     destination={trip.flight}
+                     total={trip.total}
+                     flightNumber={trip.flightNumber}
+                     airline={trip.airline}
+                     hotel={trip.hotel}/>
     )
     return (
       <div className="mdl-grid itinerary-list">
