b5ec3e2661de4b0b940bda259c1d0d32287f1542
Jacolby Green <darknightnov@gmail.com>
2018-06-01 15:55:40 -0500

    added a few comments to the model files to fully understand how to structure this application

--- models/Friends.js
+++ models/Friends.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const FriendSchema = new Schema({    
+const FriendSchema = new Schema({
     friends: [{
         type: Schema.Types.ObjectId,
         name: String,
@@ -16,3 +16,10 @@ const FriendSchema = new Schema({
 });
 
 module.exports = mongoose.model('Friend', FriendSchema);
+
+
+// You will have to obtain more information on the users of the application
+// in order to deliever the product that you want.
+
+// Figure out the fields that you will have to add to the form to in order to
+// really create a (package) for a user of for a group of traverlers.
--- models/Note.js
+++ models/Note.js
@@ -10,3 +10,11 @@ const NoteSchema = new Schema ({
 const Note = mongoose.model('Note', NoteSchema);
 
 module.exports = Note;
+
+
+
+//  Im not sure what this model does. I believe that a note is just a message
+// that a user can send to another user.
+
+
+// We will keep this model until we determine if it is useful or note.
